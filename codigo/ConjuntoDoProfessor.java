82de1d587b408507f2501b93bce1c5f9559a7363
Henrique Dani <henriquedani172004@gmail.com>
2024-06-23 15:55:31 -0300

    fix segundo conjunto do professor

--- codigo/ConjuntoDoProfessor.java
+++ codigo/ConjuntoDoProfessor.java
@@ -35,31 +35,6 @@ class ConjuntoDoProfessor {
 
     public static Conjunto gerarSegundoConjunto() {
         List<Lance> lances = new ArrayList<>();
-        lances.add(new Lance("E1", 430, 1043));
-        lances.add(new Lance("E2", 428, 1188));
-        lances.add(new Lance("E3", 410, 1565));
-        lances.add(new Lance("E4", 385, 1333));
-        lances.add(new Lance("E5", 399, 1214));
-        lances.add(new Lance("E6", 382, 1498));
-        lances.add(new Lance("E7", 416, 1540));
-        lances.add(new Lance("E8", 436, 1172));
-        lances.add(new Lance("E9", 416, 1386));
-        lances.add(new Lance("E10", 423, 1097));
-        lances.add(new Lance("E11", 400, 1463));
-        lances.add(new Lance("E12", 406, 1353));
-        lances.add(new Lance("E13", 403, 1568));
-        lances.add(new Lance("E14", 390, 1228));
-        lances.add(new Lance("E15", 387, 1542));
-        lances.add(new Lance("E16", 390, 1206));
-        lances.add(new Lance("E17", 430, 1175));
-        lances.add(new Lance("E18", 397, 1492));
-        lances.add(new Lance("E19", 392, 1293));
-        lances.add(new Lance("E20", 393, 1533));
-        lances.add(new Lance("E21", 439, 1149));
-        lances.add(new Lance("E22", 403, 1277));
-        lances.add(new Lance("E23", 415, 1624));
-        lances.add(new Lance("E24", 387, 1280));
-        lances.add(new Lance("E25", 417, 1330));
         lances.add(new Lance("E1", 313, 1496));
         lances.add(new Lance("E2", 398, 1768));
         lances.add(new Lance("E3", 240, 1210));
